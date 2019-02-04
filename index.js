93ff9dff30c327b6a013b2eb090d2f913307f7cc
VJIastelin <vjiastelin@gmail.com>
2019-02-05 00:18:40 +0500

    Some new event for gaming

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const _ = require('underscore')
 
-var port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 /*
 app.get('/',function (req,res) {
    res.sendFile(__dirname + '/index.html')
@@ -13,17 +14,37 @@ app.get('/',function (req, res) {
     res.send("Welcome to socket server");
 });
 
+var clients = {};
+
 
 
 io.on('connection', function (socket) {
     console.log('one user connected : '+socket.id);
+    //clients[socket.id] = {result: 0};
+
+
     socket.on('chat message', function (data) {
         console.log('Message is :', data)
-        io.emit('chat message', data)
     });
-    
+
+    socket.on('send result', function (data) {
+        console.log(socket.id + ' result is :', data);
+        clients[socket.id] = {result: data};
+        console.log(Object.values(clients).length);
+        if (Object.keys(clients).length > 1){
+            const winner = _.max(Object.keys(clients), function (o) {
+                return clients[o].result;
+            });
+            console.log(winner);
+            const client = io.sockets.connected[winner];
+            client.emit('send result', {result: 'You won'});
+        }
+    });
+
     socket.on('disconnect',function () {
-       console.log('user disconnected')
+        delete clients[socket.id];
+        console.log('user disconnected');
+
     });
 
 
